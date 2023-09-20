38a1bbc14db0be4bb9138f25fdc6758ea62e5b7e
anthony riachy <anthony.riachy.39@gmail.com>
2023-09-20 15:42:00 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function displaySideBar(){
+    document.getElementsByClassName('side-bar').style.display= block
+    document.getElementById,('button').style.color=red
+}
+
+function myFunction() {
+    document.getElementById,("demo").innerHTML = "Paragraph changed.";
+    document.getElementById,('side-bar').style.display= `block`
+  }
+<<<<<<< HEAD
+ //hi
+=======
+  // bye
+>>>>>>> 251f820fc936f929d2d57cc6675dab256a529003
