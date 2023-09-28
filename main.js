c6c3da73a3f3bbf73ce6f7d040f641ba55caf836
anthony riachy <anthony.riachy.39@gmail.com>
2023-09-28 12:14:42 +0300

    cv and tried the ifram for nav(fail)

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 function openSideBar() {
+    
     document.getElementById("side-bar").style.width = "250px";
   }
 
   function closeSideBar() {
     document.getElementById("side-bar").style.width= "0";
   }
+
+  
+  
+  
